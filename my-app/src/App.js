f39e8e040cf465c5929aa6253cb6f26362aea7e9
joeycrz <109363676+joeycrz@users.noreply.github.com>
2023-04-05 08:15:24 -0500

    Almost complete first mockup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,57 +1,37 @@
 //import { Text } from '@chakra-ui/react';
-import { Box, Center, ChakraBaseProvider, extendBaseTheme, Spacer } from '@chakra-ui/react';
+import { Box, Center, ChakraBaseProvider, Divider, extendBaseTheme, Spacer } from '@chakra-ui/react';
 import React from 'react';
-import { Card, CardHeader, CardBody, Text, CardFooter, Heading, Stack, StackDivider } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, Text, CardFooter, Heading, Stack } from '@chakra-ui/react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 
-
-const mainCard = {
- 
-}
 
 function App() {
   return (
     <ChakraBaseProvider>
       <div>
-        <Text fontSize={30} align="center" paddingRight={200}>Health Science Announcements Application</Text>
+        <Text fontSize={30} align="center" >Health Science Announcements Application</Text>
+        
       </div>
       <div>
-        <Card align={'center'} border='4px' marginLeft={90} marginRight={90} marginBlock={10}>
-          <CardHeader>
-            <Heading size='md'>Announcements</Heading>
-          </CardHeader>
-
-          <CardBody>
-            <Stack divider={<StackDivider />} spacing='4'>
-              <Box>
-                <Heading size='xs' textTransform='uppercase'>
-                  Summary
-                </Heading>
-                <Text pt='2' fontSize='sm'>
-                  View a summary of all your clients over the last month.
-                </Text>
-              </Box>
-              <Box>
-                <Heading size='xs' textTransform='uppercase'>
-                  Overview
-                </Heading>
-                <Text pt='2' fontSize='sm'>
-                  Check out the overview of your clients.
-                </Text>
-              </Box>
-              <Box>
-                <Heading size='xs' textTransform='uppercase'>
-                  Analysis
-                </Heading>
-                <Text pt='2' fontSize='sm'>
-                  See a detailed analysis of all your business clients.
-                </Text>
-              </Box>
-              <Spacer></Spacer>
-            </Stack>
-          </CardBody>
-        </Card>
+        <Container fluid>
+          <Row>
+            <Col align='center'>
+              <Container>
+                <div >
+                  <Text>Announcements</Text>
+                </div>
+                <div >
+                </div>
+              </Container>
+            </Col>
+          </Row>
+        </Container>
       </div>
+      
     </ChakraBaseProvider>
 
   );
